6b40518791e1cc333161230c5f4375c248fbeca0
brennanp93 <brennan.p.perez@gmail.com>
2022-12-15 19:34:23 -0800

    clean up new checklist page

--- src/components/NewCheckListPage/NewCheckListPage.jsx
+++ src/components/NewCheckListPage/NewCheckListPage.jsx
@@ -5,41 +5,31 @@ import BlankListForm from "../BlankListForm/BlankListForm"
 
 
 export default function NewCheckListPage({ checkList, addCheckListItem, deleteListItem, updateListItem, updateBoolean }) {
-// console.log(checkList, "checklist")
-    // const [updatingBoolean, setUpdatingBoolean] = useState([]);
-    // console.log(updatingBoolean)
-    // console.log(checkList)
-    // let updatedlistItem = checkList.find(listItem => listItem._id === id);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
+  function handleUpdateBoolean(idx, id) {
+    checkList[idx].completed ? checkList[idx].completed = false : checkList[idx].completed = true;
+    updateBoolean(checkList[idx], id)
+  }
 
-
-    function handleUpdateBoolean(idx, id) {
-        checkList[idx].completed ? checkList[idx].completed = false : checkList[idx].completed = true;
-        updateBoolean(checkList[idx], id)
-        console.log(checkList[idx], "here")
-    }
-
-
-
-    return (
-        <>
-            <div>
-                <BlankListForm addCheckListItem={addCheckListItem} />
-            </div>
-            <div >
-                {checkList.map((step, idx) => (
-                    <div className="step-card" key={idx}>
-                        <p>{step.stepTitle}</p>
-                        <p>{step.terminalCommand ? `Enter into Terminal:  ${step.terminalCommand}` : ''}</p>
-                        <button onClick={() => deleteListItem(step._id)}>Delete</button>
-                        <button onClick={() => navigate(`/blanklist/${step._id}/update`)}>Edit Note</button>
-                        <button onClick={() => handleUpdateBoolean(idx, step._id)} type="submit">
-                            {step.completed ? 'Undo 🔙' : 'Click to Mark as Complete ✅'}
-                        </button>
-                    </div>
-                ))}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        <BlankListForm addCheckListItem={addCheckListItem} />
+      </div>
+      <div >
+        {checkList.map((step, idx) => (
+          <div className="step-card" key={idx}>
+            <p>{step.stepTitle}</p>
+            <p>{step.terminalCommand ? `Enter into Terminal:  ${step.terminalCommand}` : ''}</p>
+            <button onClick={() => deleteListItem(step._id)}>Delete</button>
+            <button onClick={() => navigate(`/blanklist/${step._id}/update`)}>Edit Note</button>
+            <button onClick={() => handleUpdateBoolean(idx, step._id)} type="submit">
+              {step.completed ? 'Undo 🔙' : 'Click to Mark as Complete ✅'}
+            </button>
+          </div>
+        ))}
+      </div>
+    </>
+  )
 }
